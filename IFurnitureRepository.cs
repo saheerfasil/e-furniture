712ff3e18b697816de7fe65159640375a035885c
saheerfasil <46969691+saheerfasil@users.noreply.github.com>
2019-03-24 19:53:37 +0000

    Add files via upload

---
+++ IFurnitureRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFM.Models
+{
+    public interface IFurnitureRepository
+    {
+            IEnumerable<Furniture> GetAllFurniture();
+
+            Furniture GetFurnitureById(int furnitureId);
+    }
+}
