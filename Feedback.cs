bfa26fd0c4ebfa7a45a38239c958d232d66d558a
Reshmapk12 <46969854+Reshmapk12@users.noreply.github.com>
2019-03-30 13:34:31 +0000

    Create Feedback.cs

---
+++ Feedback.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DFM1.Models
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+        public bool ContactMe { get; set; }
+    }
