c2d2d040d05cfd60e93badc164d1a49a6062a957
Reshma PK <10389769@mydbs.ie>
2019-04-13 15:17:18 +0100

    update

--- Ecommerce/Ecommerce/Models/ManageViewModels.cs
+++ Ecommerce/Ecommerce/Models/ManageViewModels.cs
@@ -20,8 +20,45 @@ namespace Ecommerce.Models
         public IList<AuthenticationDescription> OtherLogins { get; set; }
     }
 
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
 
-public class AddPhoneNumberViewModel
+    public class SetPasswordViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class AddPhoneNumberViewModel
     {
         [Required]
         [Phone]
@@ -47,4 +84,3 @@ public class AddPhoneNumberViewModel
         public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
     }
 }
-   
